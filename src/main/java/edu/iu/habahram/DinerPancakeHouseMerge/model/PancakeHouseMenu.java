65a675a40b355383a7aaee01b92783fa0aaf197b
dinew18 <156202642+dinew18@users.noreply.github.com>
2024-03-27 08:21:49 -0400

    added files

--- src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/model/PancakeHouseMenu.java
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/model/PancakeHouseMenu.java
@@ -1,9 +1,11 @@
+
 package edu.iu.habahram.DinerPancakeHouseMerge.model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class PancakeHouseMenu {
+public class PancakeHouseMenu implements Menu {
     List<MenuItem> menuItems;
 
     public PancakeHouseMenu() {
@@ -51,5 +53,11 @@ public class PancakeHouseMenu {
         return  stringBuilder.toString();
     }
 
+    public Iterator<MenuItem> createIterator() {
+
+        return menuItems.iterator();
+    }
+
     // other menu methods here
 }
+
