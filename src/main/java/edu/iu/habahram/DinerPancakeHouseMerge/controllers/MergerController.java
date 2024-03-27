ffee9450aebc2387a16bcdccf3de33648bb35c3a
dinew18 <156202642+dinew18@users.noreply.github.com>
2024-03-27 09:13:23 -0400

    Add files via upload

--- src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/controllers/MergerController.java
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/controllers/MergerController.java
@@ -1,50 +1,29 @@
+
 package edu.iu.habahram.DinerPancakeHouseMerge.controllers;
 
-import edu.iu.habahram.DinerPancakeHouseMerge.model.MenuItem;
-import edu.iu.habahram.DinerPancakeHouseMerge.repository.DinerRepository;
-import edu.iu.habahram.DinerPancakeHouseMerge.repository.PancakeHouseRepository;
+import edu.iu.habahram.DinerPancakeHouseMerge.model.MenuItemRecord;
+import edu.iu.habahram.DinerPancakeHouseMerge.repository.MergerRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/merger")
 public class MergerController {
 
-    private final DinerRepository dinerRepository;
-    private final PancakeHouseRepository pancakeHouseRepository;
+    MergerRepository mergerRepository;
 
-    public MergerController(DinerRepository dinerRepository, PancakeHouseRepository pancakeHouseRepository) {
-        this.dinerRepository = dinerRepository;
-        this.pancakeHouseRepository = pancakeHouseRepository;
+    public MergerController(MergerRepository mergerRepository) {
+        this.mergerRepository = mergerRepository;
     }
 
     @GetMapping
-    public MenuItem[] getMergedMenu() {
-        List<MenuItem> mergedMenu = new ArrayList<>();
-
-        // Add diner menu items to mergedMenu
-        List<MenuItem> dinerMenuItems = List.of(dinerRepository.getTheMenu());
-        for (MenuItem menuItem : dinerMenuItems) {
-            mergedMenu.add(menuItem);
-        }
-
-        // Add pancake house menu items to mergedMenu
-        List<MenuItem> pancakeHouseMenuItems = pancakeHouseRepository.getTheMenu();
-        for (MenuItem menuItem : pancakeHouseMenuItems) {
-            mergedMenu.add(menuItem);
-        }
-
-        // Sort the merged menu items by name
-        mergedMenu.sort(Comparator.comparing(MenuItem::getName));
-
-        // Convert List<MenuItem> to MenuItem[]
-        MenuItem[] mergedMenuArray = mergedMenu.toArray(new MenuItem[mergedMenu.size()]);
-
-        return mergedMenuArray;
+    public List<MenuItemRecord> get() {
+        List<MenuItemRecord> items = mergerRepository.getTheMenuItems();
+        return items;
     }
 }
